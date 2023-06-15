27bdfb3a5791f7f64b9e6937d46c2512520cd9c7
Felipe Barbosa <fe.dominguesche@gmail.com>
2023-06-15 03:35:03 -0300

    Added button to Standalone actions

--- resources/js/asset.js
+++ resources/js/asset.js
@@ -1,7 +1,9 @@
+import ActionDropdown from './components/ActionDropdown'
 import ActionButtonSelector from './components/ActionButtonSelector'
 import InlineActionDropdown from './components/InlineActionDropdown'
 
 Nova.booting(app => {
+  app.component('ActionDropdown', ActionDropdown)
   app.component('DetailActionDropdown', ActionButtonSelector)
   app.component('InlineActionDropdown', InlineActionDropdown)
 })
